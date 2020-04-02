23a93bf6b9de509c9fef190c0a3b74f32dfef594
Diego Mendes <diego.developerx@gmail.com>
2020-04-01 23:06:45 -0300

    mobile da aplicação

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,35 +11,3 @@ app.use(routes);
 app.use(errors());
 
 app.listen(3333);
-
-
-/**
- * Rota / Recurso
- */
-
-/**
- * Métodos HTTP:
- * 
- * GET: Buscar/listar uma informação do back-end
- * POST: Criar uma informação no back-end
- * PUT: Alterar uma informação no back-end
- * DELETE: Deleter uma informação no back-end
- */
-
- /**
-  * Tipos de parâmetros:
-  * 
-  * Query: Parâmetros nomeados enviados na rota após "?" (Filtros, paginação)
-  * Route Params: Parâmetros utilizados para identificar recursos
-  * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
-  */
-
-/**
- * SQL: MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Server
- * NoSQL: MongoDB, CouchDB, etc
- */
-
- /**
-  * Driver: SELECT * FROM users
-  * Query Builder: table('users').select('*').where()
-  */
